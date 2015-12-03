f70548be3f7528d56e077c2d70a9b8ac0135c5bd
HogeTatu <hararyo.tae@gmail.com>
2015-12-04 03:48:11 +0900

    ポーズ対応UserWidget

---
+++ Source/PauseExample/PauseUserWidget.cpp
@@ -0,0 +1,34 @@
+﻿#include "PauseExample.h"
+#include "PauseUserWidget.h"
+
+UPauseUserWidget::UPauseUserWidget(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+}
+
+void UPauseUserWidget::NativeTick(const FGeometry& MyGeometry, float InDeltaTime)
+{
+	UWorld* World = GEngine->GameViewport->GetWorld();
+
+	if (World == nullptr)
+	{
+		return;
+	}
+
+	if (World->IsPaused() && !bTickEvenWhenPaused)
+	{
+		return;
+	}
+
+	UUserWidget::NativeTick(MyGeometry, InDeltaTime);
+}
+
+bool UPauseUserWidget::GetTickableWhenPaused()
+{
+	return bTickEvenWhenPaused;
+}
+
+void UPauseUserWidget::SetTickableWhenPaused(bool bTickableWhenPaused)
+{
+	bTickEvenWhenPaused = bTickableWhenPaused;
+}
---
+++ Source/PauseExample/PauseUserWidget.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+
+#include "Blueprint/UserWidget.h"
+#include "PauseUserWidget.generated.h"
+
+UCLASS(Abstract, editinlinenew, BlueprintType, Blueprintable, meta=( Category="User Controls", DontUseGenericSpawnObject="True" ) )
+class PAUSEEXAMPLE_API UPauseUserWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	UPauseUserWidget(const FObjectInitializer& ObjectInitializer);
+
+	UFUNCTION(BlueprintCallable, Category="Utilities")
+	bool GetTickableWhenPaused();
+
+	UFUNCTION(BlueprintCallable, Category="Utilities")
+	void SetTickableWhenPaused(bool bTickableWhenPaused);
+
+protected:
+	virtual void NativeTick(const FGeometry& MyGeometry, float InDeltaTime) override;
+
+public:
+	UPROPERTY(EditDefaultsOnly, Category="Tick", AdvancedDisplay)
+	bool bTickEvenWhenPaused;
+};
